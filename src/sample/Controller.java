b44f0452e52a2cda8310de7939543e18d6ee7556
rahul-madaan <madan.rahul9@gmail.com>
2021-05-26 00:35:11 +0530

    Made basic layout of first scene and implemented login sign up radio button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import javax.naming.spi.InitialContextFactory;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private RadioButton loginRadioButton;
+    @FXML private RadioButton signupRadioButton;
+    @FXML private Label loginSignupLabel;
+    private ToggleGroup loginSignupToggleGroup;
+    @FXML private Button loginSignupButton;
+    @FXML private TextField phoneNumberTextField;
+    @FXML private TextField passwordTextField;
+    @FXML private Label passwordLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loginSignupToggleGroup = new ToggleGroup();
+        this.loginRadioButton.setToggleGroup(loginSignupToggleGroup);
+        this.signupRadioButton.setToggleGroup(loginSignupToggleGroup);
+    }
+
+
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
