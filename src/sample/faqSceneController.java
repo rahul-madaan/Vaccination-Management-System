2e7a0138a6dd8e57c508b8a886e75ca9a58bd216
rahul-madaan <madan.rahul9@gmail.com>
2021-05-27 00:57:44 +0530

    added functionality to faqButton and made the window size same as main

--- src/sample/faqSceneController.java
+++ src/sample/faqSceneController.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class faqSceneController {
+
+    public void goToMainScene(ActionEvent event) throws IOException {
+        Parent scene2Parent = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
+        Scene scene2 = new Scene(scene2Parent);
+        Stage window = (Stage)((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene2);
+        window.show();
+    }
 }
