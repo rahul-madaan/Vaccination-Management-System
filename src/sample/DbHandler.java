204ad2b4f15e8bff820447093599a44dac9cb255
rahul-madaan <madan.rahul9@gmail.com>
2021-05-29 23:05:11 +0530

    reverted password and number check as it was difficult to run tests

--- src/sample/DbHandler.java
+++ src/sample/DbHandler.java
@@ -9,10 +9,10 @@ public class DbHandler {
     protected Connection connection;
 
     public Connection getConnection() {
-        final String connectionString = "jdbc:mysql://localhost:3306/vaccine_cfc";
+        final String connectionString = "jdbc:mysql://localhost:3306/Vaccine_CodeForCovid_DB";
 
         try{
-            connection = DriverManager.getConnection(connectionString,"root","Iamme123");
+            connection = DriverManager.getConnection(connectionString,"root","@2RamRam");
         }catch(SQLException ex){
             ex.printStackTrace();
         }
