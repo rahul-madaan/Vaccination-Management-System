b38f98175e991ba4e1014172561354567d19ef73
rahul-madaan <madan.rahul9@gmail.com>
2021-05-30 01:43:42 +0530

    made VaccineCentre class for saving vaccincecentres database

---
+++ src/sample/VaccineCentre.java
@@ -0,0 +1,58 @@
+package sample;
+
+public class VaccineCentre {
+    private int centreID;
+    private String hospitalName;
+    private String address;
+    private String district;
+    private String State;
+    private String pinCode;
+
+    public int getCentreID() {
+        return centreID;
+    }
+
+    public void setCentreID(int centreID) {
+        this.centreID = centreID;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
+    }
+}
