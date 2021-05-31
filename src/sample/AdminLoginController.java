f085532b5d97ce50f3738dfc9dda2ab79081e1fd
rahul-madaan <madan.rahul9@gmail.com>
2021-05-31 17:21:17 +0530

    added admin action file

--- src/sample/AdminLoginController.java
+++ src/sample/AdminLoginController.java
@@ -1,8 +1,79 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class AdminLoginController {
-    public void loginButtonClick(ActionEvent event) {
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class AdminLoginController implements Initializable {
+
+    @FXML
+    private TextField userIDTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    private Connection conn;
+
+    private DbHandler dbHandler;
+    
+    @FXML
+    private Label errorLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
+
+    public void loginButtonClick(ActionEvent event) throws SQLException, IOException {
+        String enteredPassword = passwordTextField.getText();
+        String enteredUserID = userIDTextField.getText();
+
+        String queryBoth = "SELECT * from login_admin where userID ='" + enteredUserID + "' and Password = '" + enteredPassword + "' ;";
+        conn = dbHandler.getConnection();
+        ResultSet bothSet = conn.createStatement().executeQuery(queryBoth);
+
+        String queryUserID = "SELECT * from login_admin where userID ='" + enteredUserID + "';";
+        ResultSet userIDSet = conn.createStatement().executeQuery(queryUserID);
+        if (userIDSet.next() == false) {
+            errorLabel.setVisible(true);
+            errorLabel.setText("You are not registered!");
+            errorLabel.setStyle(" -fx-background-color: #ff5c5c; -fx-text-fill: black");
+        } else if (bothSet.next() == false) {
+            errorLabel.setVisible(true);
+            errorLabel.setText("Enter correct password!");
+            errorLabel.setStyle(" -fx-background-color: #ff5c5c; -fx-text-fill: black");
+        } else {
+            errorLabel.setVisible(true);
+            errorLabel.setText("Login Successful!");
+            errorLabel.setStyle(" -fx-background-color: green; -fx-text-fill: white");
+            loginSignupButtonClickSuccess(event);
+        }
+    
+    }
+
+    public void loginSignupButtonClickSuccess(ActionEvent event) throws IOException {
+        Parent scene2Parent = FXMLLoader.load(getClass().getResource("AdminActions.fxml"));
+        Scene addMembersScene = new Scene(scene2Parent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(addMembersScene);
+        window.show();
+    }
+
 }
+
+
