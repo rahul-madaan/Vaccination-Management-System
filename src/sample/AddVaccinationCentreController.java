9c62d2bfea9f0eba5c8cac2a64b07a13a67b16af
rahul-madaan <madan.rahul9@gmail.com>
2021-06-07 06:32:33 +0530

    created UI for admin Add Centre Scene

--- src/sample/AddVaccinationCentreController.java
+++ src/sample/AddVaccinationCentreController.java
@@ -1,4 +1,30 @@
 package sample;
 
-public class AddVaccinationCentreController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddVaccinationCentreController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void backButtonClicked(ActionEvent event) throws IOException {
+        Parent scene2Parent = FXMLLoader.load(getClass().getResource("AdminActions.fxml"));
+        Scene addMembersScene = new Scene(scene2Parent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(addMembersScene);
+        window.show();
+    }
 }
