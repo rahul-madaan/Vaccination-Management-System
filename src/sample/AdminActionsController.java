f6fd0270e28533093e58a68be2157c7ba5c186b6
rahul-madaan <madan.rahul9@gmail.com>
2021-06-07 06:19:08 +0530

    added AddVaccinationCentre.fxml scene and controller

--- src/sample/AdminActionsController.java
+++ src/sample/AdminActionsController.java
@@ -65,8 +65,13 @@ public class AdminActionsController implements Initializable {
         }
     }
 
-    public void addVaccinationCentre(){
-        //add
+    @FXML
+    public void addVaccinationCentreButtonClicked(ActionEvent event) throws IOException {
+        Parent scene2Parent = FXMLLoader.load(getClass().getResource("AddVaccinationCentre.fxml"));
+        Scene addMembersScene = new Scene(scene2Parent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(addMembersScene);
+        window.show();
     }
 
     @FXML
