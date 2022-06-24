5c4c33f390a397c84c2c7540ec87a80390bbea03
Rahul Madan <34760210+rahul-madaan@users.noreply.github.com>
2022-06-25 00:44:54 +0530

    Update faqSceneController.java

--- src/sample/faqSceneController.java
+++ src/sample/faqSceneController.java
@@ -7,7 +7,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -28,7 +27,4 @@ public class faqSceneController implements Initializable {
         window.setScene(scene2);
         window.show();
     }
-
-
-
 }
